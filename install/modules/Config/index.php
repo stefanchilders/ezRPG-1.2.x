dd9a372bc659bb49dc9fd665d5b662ebe8699a62
uaktags <timgarrity89@gmail.com>
2013-09-22 12:14:01 -0400

    Removed <br /> which caused an issue

--- install/modules/Config/index.php
+++ install/modules/Config/index.php
@@ -91,7 +91,7 @@ define('SECRET_KEY', '{$secret_key}');
   DEBUG_MODE - Turn on to show database errors and debug information.
 */
 define('DB_PREFIX', '{$dbprefix}');
-define('VERSION', '1.2.1.x {UNSTABLE}');<br />
+define('VERSION', '1.2.1.x {UNSTABLE}');
 define('SHOW_ERRORS', 0);
 define('DEBUG_MODE', 0);
 ?>
